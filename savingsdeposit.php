fca67809fef6f7a33f910001b65ee8711c311d23
100LBTomato <007792350@coyote.csusb.edu>
2024-05-13 02:07:36 -0700

    Update savingsdeposit.php

--- savingsdeposit.php
+++ savingsdeposit.php
@@ -1,1 +1,17 @@
+<p>Update Savings Office Number</p>
 
+<?php
+
+
+
+$servername = "localhost";
+$username = "quickme1_4211";
+$password = "csci4211";
+$dbname = "dbvpny1qngaxgp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
