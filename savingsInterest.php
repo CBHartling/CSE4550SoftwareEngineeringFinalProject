a3b0cbead50c86112f95e18e0491b15f86b7b035
100LBTomato <007792350@coyote.csusb.edu>
2024-05-13 19:33:34 -0700

    Update savingsInterest.php

--- savingsInterest.php
+++ savingsInterest.php
@@ -1,1 +1,38 @@
+<p>Savings Deposit</p>
 
+<?php
+
+
+
+$servername = "localhost";
+$username = "quickme1_4211";
+$password = "csci4211";
+$dbname = "dbvpny1qngaxgp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+//Values for account number and the requested deposit amount
+$Acct_no = intval($_REQUEST['Acct_no']);
+
+
+//when a deposit happens you need to do both the transactions table and the savings table
+$sql = "UPDATE savings SET Balance = Balance + (Balance * interest_rate) WHERE Acct_no='$Acct_no'";
+$sql = "INSERT INTO savings_transactions (transid, trans_type, trans_date, trans_amount, lastname, firstname, phone)
+SELECT s.TRansID, 'Deposit', CURRENT_DATE(), s.Balance, s.lastname, s.firstname, s.phone
+FROM savings s
+WHERE s.Acct_no = $Acct_no;"
+
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
