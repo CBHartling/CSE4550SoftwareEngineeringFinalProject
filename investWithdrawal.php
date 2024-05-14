5f580f0ce59fbf623e6fe6617b1c77ff6e745e1a
100LBTomato <007792350@coyote.csusb.edu>
2024-05-13 20:36:37 -0700

    Update investWithdrawal.php

--- investWithdrawal.php
+++ investWithdrawal.php
@@ -18,8 +18,13 @@ if ($conn->connect_error) {
 //Values for account number and the requested deposit amount
 $Acct_no = intval($_REQUEST['Acct_no']);
 $WithAmt = floatval($_REQUEST['WithAmt']);
+$sql = "SELECT Date FROM Investment WHERE Acct_no = '$Acct_no'";
+$result = $conn->query($sql);
+$OGDate = new DateTime($result);
+$current = new DateTime();
+$interval = $current->diff($OGDate);
 
-
+if($interval->y. >=1){
 //when a deposit happens you need to do both the transactions table and the Investment table
 $sql = "UPDATE Investment SET Balance = Balance - '$WithAmt' WHERE Acct_no='$Acct_no'";
 $conn->query($sql);
@@ -27,7 +32,7 @@ $sql = "INSERT INTO investment_transactions (transid, trans_type, trans_date, tr
 SELECT s.TRansID, 'Withdrawal', CURRENT_DATE(), $WithAmt, s.lastname, s.firstname, s.phone
 FROM Investment s
 WHERE s.Acct_no = $Acct_no";
-
+}
 
 
 if ($conn->query($sql) === TRUE) {
