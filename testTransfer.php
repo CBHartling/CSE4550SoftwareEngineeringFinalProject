c15fe2b6f05c1070a169cb5b3ebc9dc74b051bcf
100LBTomato <007792350@coyote.csusb.edu>
2024-05-16 01:13:17 -0700

    Update testTransfer.php

--- testTransfer.php
+++ testTransfer.php
@@ -1,3 +1,21 @@
+<p>Checking Transfer</p>
+
+<?php
+
+
+
+$servername = "localhost";
+$username = "quickme1_4211";
+$password = "csci4211";
+$dbname = "dbvpny1qngaxgp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+//Values for account number and the requested deposit amount
 $conn->begin_transaction();
 
 try {
@@ -24,3 +42,12 @@ try {
     $conn->rollback();
     echo "Transaction failed: " . $e->getMessage();
 }
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
